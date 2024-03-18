3b0ec7c49f7ca3d4a68c509c3bd2927980225b03
rRedq <38762919+rRedq@users.noreply.github.com>
2024-03-19 01:39:06 +0500

    refactor: RSS-PZ-07 add animation in/out

--- rss-puzzle/src/app/components/start-page/start-page.ts
+++ rss-puzzle/src/app/components/start-page/start-page.ts
@@ -30,14 +30,18 @@ export default class StartPage extends CreateElement {
     this.greet.textContent(`Hello, ${nameAndSurname?.firstName} ${nameAndSurname?.surName}, nice to meet you!`);
     this.container.appendChildren([this.h2, this.greet, this.text, this.button]);
     this.elementAppend(this.container);
-    this.button.addEventListener('click', this.closeStartPage.bind(this));
+    this.button.addEventListener('click', this.closeStartPage);
   }
 
-  private closeStartPage(): void {
+  private closeStartPage = (): void => {
     this.button.removeEventListener('click', this.closeStartPage);
-    this.removeNode();
-    const lastGame = getStorage<CurrentWord>('lastGame');
-    if (lastGame) this.app.startGame(lastGame, true);
-    else this.app.startGame(lastGame);
-  }
+    this.addClass('start-page__close');
+
+    setTimeout(() => {
+      this.removeNode();
+      const lastGame = getStorage<CurrentWord>('lastGame');
+      if (lastGame) this.app.startGame(lastGame, true);
+      else this.app.startGame(lastGame);
+    }, 900);
+  };
 }
